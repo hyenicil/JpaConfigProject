c5c458976deba72511a7ac702445a85ed9460a12
hyenicil <57847728+hyenicil@users.noreply.github.com>
2024-07-01 14:06:58 +0300

    Started project.

---
+++ src/main/java/org/yenicilh/jpaconfigproject/JpaConfigProjectApplication.java
@@ -0,0 +1,13 @@
+package org.yenicilh.jpaconfigproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaConfigProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaConfigProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/yenicilh/jpaconfigproject/JpaConfigProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yenicilh.jpaconfigproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaConfigProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
